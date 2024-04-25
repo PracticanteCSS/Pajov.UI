adbda30575afd3dd1387ef001832c1920d7b9d11
Practicante CSS <127709075+PracticanteCSS@users.noreply.github.com>
2024-04-25 09:15:27 -0500

    package instalado y componentes creados

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    LayoutComponent
+  ],
   imports: [
     CommonModule,
     LayoutRoutingModule
